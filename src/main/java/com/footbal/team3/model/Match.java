262b03990df0883960ef87a79f496d9940635a51
Edvaldo vunge <53275378+edvunge@users.noreply.github.com>
2021-10-19 12:13:20 +0100

    FT-CreateModelMatch

---
+++ src/main/java/com/footbal/team3/model/Match.java
@@ -0,0 +1,39 @@
+package com.footbal.team3.model;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@ToString
+@Builder
+@Document
+public class Match {
+    @Id
+    String id;
+
+    private String description;
+    private Date matchDate;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
