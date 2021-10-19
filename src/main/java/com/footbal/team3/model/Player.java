2435ef16ad9e03b60be2257efbe1d9dcaa298d33
alvarotaveirapinto <alvarotaveirapinto@gmail.com>
2021-10-19 10:35:38 +0100

    FT- Player model-FT

---
+++ src/main/java/com/footbal/team3/model/Player.java
@@ -0,0 +1,59 @@
+package com.footbal.team3.model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+public class Player {
+    @javax.persistence.Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long playerId;
+    private int playerNumber;
+    private String playerPosition;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Player(Long playerId, int playerNumber, String playerPosition) {
+        this.playerId = playerId;
+        this.playerNumber = playerNumber;
+        this.playerPosition = playerPosition;
+    }
+
+    public Long getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Long playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public String getPlayerPosition() {
+        return playerPosition;
+    }
+
+    public void setPlayerPosition(String playerPosition) {
+        this.playerPosition = playerPosition;
+    }
+}
